1410e128d989c2db5ccbf782e3570a7250bcd81c
Everlyn <gachuieve32@gmail.com>
2023-09-27 20:00:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// // code your solution here
+const record = [
+  { 
+    year: "2015", 
+    result: "W"
+},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"},
+  
+]
+
+
+function superbowlWin(record) {
+  const outcome = record.find(key => key.result === "W");
+ 
+  return outcome? outcome.year : undefined;
+}
+superbowlWin(record);
